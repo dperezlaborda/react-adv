99835bf2606107fc69d5263665610efb4a12ddab
Dolores Perez Laborda <doloresperezlaborda@gmail.com>
2022-11-09 12:47:21 -0300

    se agrega router en navigation

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,35 +1,32 @@
-import { BrowserRouter, Route, NavLink, Redirect, Switch } from 'react-router-dom';
+import { Route, NavLink, Redirect, Switch, Router } from 'react-router-dom';
 import logo from '../logo.svg';
 // import { LazyPage1, LazyPage2, LazyPage3 } from '../lazyload/pages';
 import { routes } from './routes';
-import { Suspense } from 'react';
 // import { LazyPage1, LazyPage2, LazyPage3 } from '../lazyload/pages';
 
 const Navigation = () => {
 
   return (
-    <Suspense fallback={<span>Loading...</span>}>
-        <BrowserRouter>
-            <div className="main-layout">
-                <nav>
-                    <img src={logo} alt="React logo" />
-                    <ul>
-                        {routes.map((route, index) => (
-                            <li key={index}>
-                                <NavLink to={route.path} className={({ isActive }) => isActive ? 'nav-active' : '' }>{route.name}</NavLink>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-                <Switch>
-                    {routes.map(({ path, Component }) => (
-                        <Route key={path} path={path} render={() => <Component />} />
+    <Router>
+           <div className="main-layout">
+            <nav>
+                <img src={logo} alt="React logo" />
+                <ul>
+                    {routes.map((route, index) => (
+                        <li key={index}>
+                            <NavLink to={route.path} className={({ isActive }) => isActive ? 'nav-active' : '' }>{route.name}</NavLink>
+                        </li>
                     ))}
-                    <Redirect to={routes[0].path} />
-                </Switch>
-            </div>
-        </BrowserRouter>
-    </Suspense>
+                </ul>
+            </nav>
+            <Switch>
+                {routes.map(({ path, Component }) => (
+                    <Route key={path} path={path} render={() => <Component />} />
+                ))}
+                <Redirect to={routes[0].path} />
+            </Switch>
+        </div>
+    </Router>
   )
 }
 
